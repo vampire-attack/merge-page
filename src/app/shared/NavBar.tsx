7f8d663716949ef9fd33eef14f56d00556df1ad4
assasinooz77 <assasinooz77@gmail.com>
2024-07-03 20:31:37 +0800

    feat: update navbar mobile styles

--- src/app/shared/NavBar.tsx
+++ src/app/shared/NavBar.tsx
@@ -20,24 +20,9 @@ export default function NavBar() {
 
   return (
     <header>
-      <nav className="px-4 lg:px-6 py-2.5 mt-2">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-2xl">
-          <div className="flex items-center lg:order-2">
-            <ConnectButton label="Connect" />
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-            </button>
-          </div>
-          <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
-          >
+      <nav className="px-4 lg:px-6 py-2.5">
+        <div className="flex justify-between items-center mx-auto max-w-screen-2xl">
+          <div className="flex items-center gap-4">
             <Link href="#" className="flex items-center">
               <Image
                 src={logo}
@@ -46,7 +31,7 @@ export default function NavBar() {
                 alt="vampire-attack"
               />
             </Link>
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 ps-8">
+            <ul className="items-center gap-2 hidden md:flex">
               <li>
                 <Link
                   href="/merge"
@@ -58,6 +43,8 @@ export default function NavBar() {
               </li>
             </ul>
           </div>
+
+          <ConnectButton label="Connect" />
         </div>
       </nav>
     </header>
