40458e5ed644f0755343ea4dc2ae84247c405d30
assasinooz77 <assasinooz77@gmail.com>
2024-07-08 17:55:16 +0800

    feat: add merge write hooks

--- src/hooks/useMerge.ts
+++ src/hooks/useMerge.ts
@@ -1,7 +1,13 @@
-import { useReadContract } from "wagmi";
+import {
+  useReadContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
 import { CONTRACT_ADDRESSES } from "@/constants";
 import { MergeABI } from "@/lib/abis";
 import { MergeInfo } from "@/types";
+import { useEffect } from "react";
+import { notifications } from "@mantine/notifications";
 
 export const useMergeAssetAddress = () => {
   const { data, isFetching } = useReadContract({
@@ -80,5 +86,121 @@ export const useReleasedAmount = (
   return {
     data: data ?? 0n,
     isFetching,
+  };
+};
+
+export const useDeposit = () => {
+  const {
+    data: hash,
+    error: errorCreation,
+    isPending: isTxCreating,
+    isError: isCreationError,
+    writeContractAsync,
+  } = useWriteContract();
+  const {
+    isLoading: isTxConfirming,
+    error: errorConfirm,
+    isError: isConfirmError,
+    isSuccess: isConfirmed,
+  } = useWaitForTransactionReceipt({ hash });
+
+  useEffect(() => {
+    if (isConfirmed) {
+      notifications.show({
+        title: "Deposit",
+        message: "Deposited successfully!",
+      });
+    }
+    if (isCreationError) {
+      notifications.show({
+        title: "Deposit",
+        message: "Deposit failed!",
+      });
+      console.log(errorCreation?.message);
+    }
+    if (isConfirmError) {
+      notifications.show({
+        title: "Deposit",
+        message: "Deposit failed!",
+      });
+      console.log(errorConfirm?.message);
+    }
+  }, [isConfirmed, isCreationError, isConfirmError]);
+
+  const onWriteAsync = async (
+    targetAsset: `0x${string}`,
+    targetAmount: bigint
+  ) => {
+    await writeContractAsync({
+      abi: MergeABI,
+      address: CONTRACT_ADDRESSES.merge,
+      functionName: "deposit",
+      args: [targetAsset, targetAmount],
+    });
+  };
+
+  return {
+    isPending: isTxCreating || isTxConfirming,
+    hash,
+    onWriteAsync,
+    isConfirmed,
+  };
+};
+
+export const useWithdraw = () => {
+  const {
+    data: hash,
+    error: errorCreation,
+    isPending: isTxCreating,
+    isError: isCreationError,
+    writeContractAsync,
+  } = useWriteContract();
+  const {
+    isLoading: isTxConfirming,
+    error: errorConfirm,
+    isError: isConfirmError,
+    isSuccess: isConfirmed,
+  } = useWaitForTransactionReceipt({ hash });
+
+  useEffect(() => {
+    if (isConfirmed) {
+      notifications.show({
+        title: "Withdraw",
+        message: "Withdrew successfully!",
+      });
+    }
+    if (isCreationError) {
+      notifications.show({
+        title: "Withdraw",
+        message: "Withdraw failed!",
+      });
+      console.log(errorCreation?.message);
+    }
+    if (isConfirmError) {
+      notifications.show({
+        title: "Withdraw",
+        message: "Withdraw failed!",
+      });
+      console.log(errorConfirm?.message);
+    }
+  }, [isConfirmed, isCreationError, isConfirmError]);
+
+  const onWriteAsync = async (
+    targetAsset: `0x${string}`,
+    mergeAmount: bigint
+  ) => {
+    await writeContractAsync({
+      abi: MergeABI,
+      address: CONTRACT_ADDRESSES.merge,
+      functionName: "withdraw",
+      args: [targetAsset, mergeAmount],
+    });
+  };
+
+  return {
+    isPending: isTxCreating || isTxConfirming,
+    hash,
+    onWriteAsync,
+    isConfirmed,
   };
 };
