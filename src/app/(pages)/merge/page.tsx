995cfd46cd3843c8211582af15515542dd040e5d
assasinooz77 <assasinooz77@gmail.com>
2024-07-09 20:54:40 +0800

    feat: read target assets from contract

--- src/app/(pages)/merge/page.tsx
+++ src/app/(pages)/merge/page.tsx
@@ -1,9 +1,11 @@
 import { MergeCard } from "@/components";
-import { MergeStatus } from "@/types";
-import { Text, Title } from "@mantine/core";
+import { useTargetAssets } from "@/hooks";
+import { LoadingOverlay, Text, Title } from "@mantine/core";
 import React from "react";
 
 export default async function MergePage() {
+  const { data: targetAssets, isFetching } = useTargetAssets();
+
   return (
     <div className="flex flex-col items-center w-full md:w-[486px] lg:w-[992px]">
       <div className="bg-vampire-light card px-5 py-2 w-full">
@@ -17,11 +19,19 @@ export default async function MergePage() {
       </div>
 
       <div className="flex flex-wrap lg:grid-cols-2 mt-5 gap-5">
-        <MergeCard />
-        <MergeCard status={MergeStatus.Deposit} />
-        <MergeCard status={MergeStatus.Cliff} />
-        <MergeCard status={MergeStatus.Vesting} />
-        <MergeCard />
+        <LoadingOverlay
+          visible={isFetching}
+          zIndex={1000}
+          overlayProps={{ radius: "sm", blur: 2 }}
+        />
+
+        {!isFetching && targetAssets.length === 0 ? (
+          <Text size="md">No Target Assets</Text>
+        ) : (
+          targetAssets.map((targetAsset, index) => (
+            <MergeCard targetAsset={targetAsset} key={index} />
+          ))
+        )}
       </div>
     </div>
   );
